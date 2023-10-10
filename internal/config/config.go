c12a7ca01831b7fcdd0960d9d0504bd85a1482a4
Ell-se <143798346+Ell-se@users.noreply.github.com>
2023-10-10 11:06:28 +0300

    add config server vars

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,15 @@ package config
 
 import (
 	"flag"
+	"log"
+
+	"github.com/caarlos0/env/v6"
 )
+
+type Config struct {
+	ServerAddress string `env:"SERVER_ADDRESS"`
+	BaseUrl       string `env:"BASE_URL"`
+}
 
 var FlagRunAddr string
 var FlagShortAddr string
@@ -11,11 +19,23 @@ var Host string
 // parseFlags обрабатывает аргументы командной строки
 // и сохраняет их значения в соответствующих переменных
 func ParseFlags() {
-
+	var cfg Config
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 	Host = "http://localhost"
 	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&FlagShortAddr, "b", "http://localhost:8080", "address and port to short")
 
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
+	// выбираем значения
+	if cfg.BaseUrl != "" {
+		FlagShortAddr = cfg.BaseUrl
+	}
+	if cfg.ServerAddress != "" {
+		FlagRunAddr = cfg.ServerAddress
+	}
+
 }
